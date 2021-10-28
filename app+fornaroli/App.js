5ae391fa1c05ac34363f6918b2d21cdea68e4ed8
mf-fornaroli <93348952+mf-fornaroli@users.noreply.github.com>
2021-10-28 17:04:36 -0300

    Add files via upload

---
+++ app+fornaroli/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Coder!</Text>
+      <Text>Soy Mauro Fornaroli y esta es mi primera app en React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
